16c7f2b44149c261802559fb52f85e536b0437a5
Dmitriy <dmitriyinfo@gmail.com>
2022-08-31 10:43:31 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+
+    }
+
+    public void printInfo() {
+        System.out.println("active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error hardware = new Error();
+        Error network = new Error(true, 404, "connection lost");
+        Error server = new Error(true, 500, "unauthorized");
+        hardware.printInfo();
+        network.printInfo();
+        server.printInfo();
+    }
+}
